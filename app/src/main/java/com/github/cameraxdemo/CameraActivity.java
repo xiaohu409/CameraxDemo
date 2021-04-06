608466b1e88da6d65276bfb50d80d6f1bd80a486
cdfy <285279904@qq.com>
2021-04-06 13:57:51 +0800

    1.重构代码

--- app/src/main/java/com/github/cameraxdemo/CameraActivity.java
+++ app/src/main/java/com/github/cameraxdemo/CameraActivity.java
@@ -1,8 +1,11 @@
 package com.github.cameraxdemo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -10,28 +13,55 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ProcessCameraProvider cameraProvider;
     private int defaultCamera = CameraSelector.LENS_FACING_BACK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         initView();
+        bindData();
     }
 
     private void initView() {
+        previewView = findViewById(R.id.preview_view);
+        Button switchBtn = findViewById(R.id.switch_btn_id);
+        switchBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (defaultCamera == CameraSelector.LENS_FACING_BACK) {
+            defaultCamera = CameraSelector.LENS_FACING_FRONT;
+        }
+        else {
+            defaultCamera = CameraSelector.LENS_FACING_BACK;
+        }
+        bindPreview(cameraProvider);
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(defaultCamera)
+                .build();
+        Preview preview = new Preview.Builder()
+                .build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+    }
+
+    private void bindData() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(new Runnable() {
 
@@ -46,31 +76,5 @@ public class CameraActivity extends AppCompatActivity {
                 }
             }
         }, ContextCompat.getMainExecutor(this));
-
-        Button btn = findViewById(R.id.switch_btn_id);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (defaultCamera == CameraSelector.LENS_FACING_BACK) {
-                    defaultCamera = CameraSelector.LENS_FACING_FRONT;
-                }
-                else {
-                    defaultCamera = CameraSelector.LENS_FACING_BACK;
-                }
-                bindPreview(cameraProvider);
-            }
-        });
     }
-
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(defaultCamera)
-                .build();
-        Preview preview = new Preview.Builder()
-                .build();
-        PreviewView previewView = findViewById(R.id.preview_view);
-        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
-        preview.setSurfaceProvider(previewView.createSurfaceProvider());
-    }
-
 }
